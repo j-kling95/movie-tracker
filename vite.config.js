5ff179b02aa64580718a6d1cf407ef19492e4718
Julian Kling <julian.kling@mail-me.net>
2026-04-16 21:48:39 +0200

    push deletions

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import dsv from "@rollup/plugin-dsv";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), dsv()],
-});
